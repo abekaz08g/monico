91d76ea351d875ca0fe183830d7d31c9d3260a33
abekaz08g <kazuya.abe@gmail.com>
2014-06-09 12:57:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
 #-*- coding:utf-8 -*-
 
-import feedparser
-import urllib
-from bs4 import BeautifulSoup
-import treetagger as tagger
+import conf
+import proc
 
 
-def parseRss(url):
-    rss = feedparser.parse(url)
-    links = []
-    for entry in rss['entries']:
-        links.append(entry['link'])
-    return links
-
-
-def getArticle(link):
-    suffix = u'/komplettansicht'
-    f = urllib.urlopen(link + suffix)
-    cont = f.read()
-    f.close()
-    return cont
-
-
-def parseArticle(article):
-    soup = BeautifulSoup(article)
-    text = ''
-    for div in soup.find_all('div'):
-        if div.has_attr('class') and div['class'] == [u'article-body']:
-            text += div.get_text()
-    return text
-
-
-def treetagger(str):
-    return tagger.run(str)
-
-
-def textToSentences(antText):
-    """
-    antText: unicode text annotated by treetagger
-    """
-    sentences = []
-    sentence = []
-    for word in antText:
-        sentence.append(word)
-        if word[1] in [u'$.', u'$?', u'$!']:
-            antSent = {u'surface': [], u'pos': [], u'lemma': []}
-            for w in sentence:
-                antSent[u'surface'].append(w[0])
-                antSent[u'pos'].append(w[1])
-                antSent[u'lemma'].append(w[2])
-            sentences.append(antSent)
-            sentence = []
-    return sentences
+DATABASE = proc.database(conf.DBCONF)
+DATABASE.connect()
+DATABASE.fetchArticles()
+print 'finished!'
+DATABASE.disconnect()
